a0c72c7a3d9f231675e02319bc6983cf11da4a22
Jérémy <rcd.jeremy@gmail.com>
2024-12-13 15:16:57 +0100

    :tada: Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tech.altab.sofubo',
+  appName: 'sofubo',
+  webDir: './dist'
+};
+
+export default config;
