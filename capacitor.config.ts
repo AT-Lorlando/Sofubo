ab1b3baa7551b573937d2b690df0623c1b2ece2f
Jérémy <rcd.jeremy@gmail.com>
2024-12-16 20:55:25 +0100

    :sparkles: Statics app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'tech.altab.sofubo',
-  appName: 'sofubo',
-  webDir: './dist'
+  appId: "tech.altab.sofubo",
+  appName: "sofubo",
+  webDir: ".output/public",
 };
 
 export default config;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  target: "static",
   runtimeConfig: {
     public: {
       apiBaseUrl:
         process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
     },
   },
+  generate: { fallback: true },
 });
